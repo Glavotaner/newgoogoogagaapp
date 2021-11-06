c3768bee24fd99f531340156236af0c36d004f36
glavotaner <marin.glavas@outlook.com>
2021-11-06 20:52:17 +0100

    fix scaffold

--- lib/screens/scaffold.dart
+++ lib/screens/scaffold.dart
@@ -37,58 +37,58 @@ class _ScaffoldScreenState extends State<ScaffoldScreen> {
 
   @override
   Widget build(BuildContext context) {
-    bool _refreshDisabled =
-        context.watch<UsersManager>().usersData[User.me]?.token == null;
-
-    return Scaffold(
-      appBar: AppBar(
-          leading: _selectedTab == 1
-              ? Padding(
-                  padding: const EdgeInsets.only(left: 8.0),
-                  child: IconButton(
-                    color: Theme.of(context).cardColor,
-                    onPressed: () => clearArchive(context),
-                    icon: Icon(Icons.delete_forever),
-                    alignment: Alignment.centerLeft,
-                  ),
-                )
-              : null,
-          actions: [
-            IconButton(
-                color: Theme.of(context).cardColor,
-                onPressed: () =>
-                    Provider.of<AppStateManager>(context, listen: false)
-                        .enterUsersSetUp(true),
-                icon: Icon(Icons.people)),
-            IconButton(
-                color: Colors.redAccent,
-                onPressed:
-                    _refreshDisabled ? null : () => refreshBabyToken(context),
-                icon: Icon(
-                    context.watch<UsersManager>().usersData[User.baby]?.token !=
-                            null
-                        ? Icons.favorite
-                        : Icons.favorite_border))
-          ],
-          title: const Text('Googoo Gaga App'),
-          bottomOpacity: 0,
-          centerTitle: true),
-      bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _selectedTab,
-          selectedFontSize: 16.0,
-          showSelectedLabels: true,
-          showUnselectedLabels: false,
-          onTap: _selectTab,
-          items: [
-            BottomNavigationBarItem(icon: Icon(Icons.send), label: 'Send kiss'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.archive_sharp), label: 'Kiss archive')
-          ]),
-      backgroundColor: Colors.white,
-      body: IndexedStack(
-        index: _selectedTab,
-        children: widget.pages,
-      ),
-    );
+    return Consumer3<AppStateManager, UsersManager, ArchiveManager>(builder:
+        (context, appStateManager, usersManager, archiveManager, child) {
+      final _showClearArchiveIcon =
+          _selectedTab == 1 && archiveManager.messages.isNotEmpty;
+      return Scaffold(
+        appBar: AppBar(
+            leading: _showClearArchiveIcon
+                ? Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: IconButton(
+                      color: Theme.of(context).cardColor,
+                      onPressed: () => clearArchive(context),
+                      icon: Icon(Icons.delete_forever),
+                      alignment: Alignment.centerLeft,
+                    ),
+                  )
+                : null,
+            actions: [
+              IconButton(
+                  color: Theme.of(context).cardColor,
+                  onPressed: () => appStateManager.enterUsersSetUp(true),
+                  icon: Icon(Icons.people)),
+              IconButton(
+                  color: Colors.redAccent,
+                  onPressed: usersManager.usersData[User.me]?.token == null
+                      ? null
+                      : () => refreshBabyToken(context),
+                  icon: Icon(usersManager.usersData[User.baby]?.token == null
+                      ? Icons.favorite_border
+                      : Icons.favorite))
+            ],
+            title: const Text('Googoo Gaga App'),
+            bottomOpacity: 0,
+            centerTitle: true),
+        bottomNavigationBar: BottomNavigationBar(
+            currentIndex: _selectedTab,
+            selectedFontSize: 16.0,
+            showSelectedLabels: true,
+            showUnselectedLabels: false,
+            onTap: _selectTab,
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.send), label: 'Send kiss'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.archive_sharp), label: 'Kiss archive')
+            ]),
+        backgroundColor: Colors.white,
+        body: IndexedStack(
+          index: _selectedTab,
+          children: widget.pages,
+        ),
+      );
+    });
   }
 }
